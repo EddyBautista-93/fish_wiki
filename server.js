80c02b061f37b77bbc7e61ab95e1c4b91c2f3bed
EddyBautista <bautista.j.eddy@gmail.com>
2022-08-05 21:18:01 -0500

    add axios

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+async function fetchAPIData(species) {
+    const apiResponse = await axios.get(
+        `https://www.fishwatch.gov/api/species/${species}`
+    );
+    console.log("Request sent to the API");
+    return apiResponse.data;
+}
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
