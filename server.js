179bb475cc7ebeb887bfb0158d96c30d3229e7c1
EddyBautista <bautista.j.eddy@gmail.com>
2022-08-08 06:34:40 -0500

    set up redis cacheing inside of the app with logic on returning cache / api data

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const axios = require("axios");
+const redis = require("redis");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+let redisClient;
+
+// anonymous self-invoked async function invoking the
+// createClient method. 
+// By default Redis uses port 6379
+(async () => {
+  redisClient = redis.createClient();
+
+  redisClient.on("error", (error) => console.error(`Error : ${error}`));
+
+  await redisClient.connect();
+})();
+
+// function that grabs the data from fishwatch API.
 async function fetchApiData(species) {
   const apiResponse = await axios.get(
     `https://www.fishwatch.gov/api/species/${species}`
@@ -15,14 +30,26 @@ async function fetchApiData(species) {
 async function getSpeciesData(req, res) {
   const species = req.params.species;
   let results;
+  let isCached = false;
 
+  // if the data is inside of the redis cached store we change isCached
+  // to true and return the data from there instead of calling the fetchApi Data function
   try {
-    results = await fetchApiData(species);
-    if (results.length === 0) {
-      throw "API returned an empty array";
+    const cacheResults = await redisClient.get(species);
+    if (cacheResults) {
+      isCached = true;
+      results = JSON.parse(cacheResults);
+    } else {
+      results = await fetchApiData(species);
+      if (results.length === 0) {
+        throw "API returned an empty array";
+      }
+      // save store the data in Redis Cache
+      await redisClient.set(species, JSON.stringify(results));
     }
+    // return the results from the get method
     res.send({
-      fromCache: false,
+      fromCache: isCached,
       data: results,
     });
   } catch (error) {
@@ -31,6 +58,7 @@ async function getSpeciesData(req, res) {
   }
 }
 
+// API endpoint
 app.get("/fish/:species", getSpeciesData);
 
 app.listen(port, () => {
